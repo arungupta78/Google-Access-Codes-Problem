bf8e191f909a416b62f9f981b69c278ddf259eb7
Arun Kumar Gupta <31176941+arungupta78@users.noreply.github.com>
2020-05-09 12:53:45 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,1 +1,41 @@
+import java.util.Arrays;
+
+public class Solution {
+
+	public static int solution(int[] l) {
+		Arrays.sort(l);
+
+		int count = 0;
+		for (int i = 0; i < l.length; i++) {
+			for (int j = i + 1; j < l.length; j++) {
+
+				if (l[j] % l[i] == 0) {
+					count = count + foundFirst(l, j, l[j]);
+				}
+			}
+		}
+		return count;
+	}
+
+	private static int foundFirst(int[] l, int j, int value) {
+		int count = 0;
+		for (int x = j + 1; x < l.length; x++) {
+
+			if (l[x] % value == 0) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] x = { 1, 2, 3, 4, 5, 6 };
+		int[] y = { 1, 1, 1 };
+
+		int result = solution(y);
+		System.out.println(result);
+	}
+
+}
 
